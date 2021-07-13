548bf89ebfff1259e323c987acc72746ec100fc7
Zach Stone <zachinjapan888s@gmail.com>
2021-07-13 10:37:09 +0900

    added animation and sounds to users clicks

--- game.js
+++ game.js
@@ -1,12 +1,24 @@
 // color array
 const buttonColors = ["red", "blue", "green", "yellow"];
 
-// game pattern
+//  array for the game pattern
 const gamePattern = []
 
 // array containing what the user clicks
 const userClickedPattern = [];
 
+// function that plays sound for user clicked color
+function playSound(name) {
+    var userClickedButtonSound = new Audio("sounds/" + name + ".mp3");
+    userClickedButtonSound.play();
+}
+
+// function that animates user's clicks
+function animatePress(currentColor) {
+    $("#" + currentColor).addClass("pressed");
+}
+
+// picks a color randomlly, adds it to an array, and animates it
 function nextSequence() {
     //function containing a random number generator from 0-3
     var randomNumber = Math.floor((Math.random() * 4));
@@ -19,20 +31,28 @@ function nextSequence() {
     var sequencedButtonSound = new Audio("sounds/" + randomChosenColor + ".mp3");
     sequencedButtonSound.play();
 }
-// build up an array of click colors (buttons)
+
+
+// interactive part
+
+// -----------------------------------
+
+// build up an array of the user's click colors (buttons), plays a sound, and animates
 $(".btn").on("click", function () {
-    var userChoosenColor = this.attr("id");
-    console.log(userChoosenColor);
-    // userClickedPattern.push(userChoosenColor);
-    // console.log(userClickedPattern);
+    var userChoosenColor = $(this).attr("id", );
+    userClickedPattern.push(userChoosenColor);
+    console.log(userClickedPattern);
+    playSound(userChoosenColor);
+    // animate function
+    animatePress(userChoosenColor);
+    // time out function to undo animation
+
+    setTimeout(function () {
+        $("#" + userChoosenColor).removeClass("pressed");
+    }, 100);
+
 })
 
 
+
 nextSequence();
-console.log(gamePattern);
-// nextSequence();
-// console.log(gamePattern);
-// nextSequence();
-// console.log(gamePattern);
-// nextSequence();
-// console.log(gamePattern);
